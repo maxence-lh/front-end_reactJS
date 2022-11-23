23214504f319bfcb04d1b7700a3c5e8fd2848eae
maxence-lh <lh.maxence@live.fr>
2022-11-23 16:46:47 +0100

    Props

--- tuto_video/hello-world/src/App.js
+++ tuto_video/hello-world/src/App.js
@@ -6,9 +6,19 @@ import Hello from './components/Hello';
 function App() {
   return (
     <div className="App">
-      {/* <Greet/>
-      <Welcome/> */}
-      <Hello />
+      <Greet name="Bruce" heroName="Batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Woman" />
+
+      <Welcome name="Bruce" heroName="Batman"/>
+      <Welcome name="Clark" heroName="Superman"/>
+      <Welcome name="Diana" heroName="Wonder Woman" />
+      
+      {/* <Hello /> */}
     </div>
   );
 }
